0aa102038c45756703a0af9bd95a19b4e786e1bf
Erik Lott <mreriklott@gmail.com>
2018-09-04 13:30:43 -0400

    add filelist to list function

--- src/file_list.ml
+++ src/file_list.ml
@@ -2,3 +2,15 @@ type t
 
 external length : t -> int = "length" [@@bs.get]
 external item: t -> int -> File.t Js.Nullable.t = "item" [@@bs.send]
+
+let to_list files =
+  let num = length files in
+  let rec append index list =
+    if index < num then
+      match Js.Nullable.toOption (item files index) with
+      | Some file -> file :: (append (index + 1) list)
+      | None -> list
+    else
+      list
+  in
+  append 0 []
--- src/file_list.mli
+++ src/file_list.mli
@@ -2,3 +2,4 @@ type t
 
 val length : t -> int
 val item : t -> int -> File.t Js.Nullable.t
+val to_list : t -> File.t list
